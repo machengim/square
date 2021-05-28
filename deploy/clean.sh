43a7f229824e0b42710a4a4cbd5ad4af02c1f0f6
Cheng <machengiam@gmail.com>
2021-05-28 21:20:44 +1000

    backup deployment config

---
+++ deploy/clean.sh
@@ -0,0 +1,3 @@
+[ -d "./data/postgres" ] && sudo rm -r ./data/postgres
+[ -d "./data/redis" ] && sudo rm -r ./data/redis
+
---
+++ deploy/frontend/precache-manifest.0f74e0dd15259f60f4ab213e5086b304.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eb25bceca8f0a435fd35666d286e1b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93a47427491f3003b59a",
+    "url": "/static/css/main.a26d4756.chunk.css"
+  },
+  {
+    "revision": "53146ff334c86f4c260a",
+    "url": "/static/js/2.c5b75307.chunk.js"
+  },
+  {
+    "revision": "6b3248a1692933689dcb27b9e1ed5092",
+    "url": "/static/js/2.c5b75307.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93a47427491f3003b59a",
+    "url": "/static/js/main.3ab50f9a.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
